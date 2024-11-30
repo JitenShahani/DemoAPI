79f5146a01cc1b9ea4a8e9603ebe7a4088b28072
Jiten Shahani <shahani.jiten@gmail.com>
2024-11-30 12:54:23 +0530

    Moved using statement to GlobalUsings.cs

--- DemoAPI/GlobalUsings.cs
+++ DemoAPI/GlobalUsings.cs
@@ -4,4 +4,5 @@ global using System.Net;
 global using DemoAPI;
 global using Microsoft.AspNetCore.Http.HttpResults;
 global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.OpenApi.Models;
 global using Scalar.AspNetCore;
